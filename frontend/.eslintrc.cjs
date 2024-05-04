7d43ee95c98ff73161b173459cef489f98479bf5
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-05-04 17:10:41 +0900

    Update frontend configuration files

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,39 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'plugin:react/recommended',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		// react17以降はReactのimport不要
+		'react/react-in-jsx-scope': 'off',
+		// react-hooks
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		// 改行コード
+		'linebreak-style': ['error', 'unix'],
+		// import文の順序
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+				// importグループごとに改行する
+				'newlines-between': 'always',
+				// 各グループ内の順序をアルファベット順に並べ替える
+				alphabetize: { order: 'asc', caseInsensitive: true },
+			},
+		],
+	},
+};
